e58a532e75c8b767e67f2ea4d16ed8677043b090
mruisan <mruisa@outlook.es>
2026-03-25 12:51:51 +0100

    Add files via upload

---
+++ Ejercicios_Repaso/test_ejercicio_4.py
@@ -0,0 +1,9 @@
+
+from Ejercicio_4_repaso import ConversorTemperatura
+
+temperatura_1=ConversorTemperatura(45,"Fahrenheit")
+temperatura_2=ConversorTemperatura(23,"Celsius")
+
+temperatura_1.conversor()
+temperatura_2.conversor()
+
